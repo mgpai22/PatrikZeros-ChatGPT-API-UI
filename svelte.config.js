a8e2bb5f93b5914b3f417fffb12dc8315bb55301
mgpai <64056966+mgpai22@users.noreply.github.com>
2023-03-17 23:12:12 -0400

    netlify fix

--- svelte.config.js
+++ svelte.config.js
@@ -3,12 +3,16 @@ import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  // Consult https://kit.svelte.dev/docs/integrations#preprocessors
-  // for more information about preprocessors
   preprocess: vitePreprocess(),
 
   kit: {
-    adapter: adapter()
+    adapter: adapter({
+      out: 'build', // update the output directory
+    }),
+    files: {
+      assets: 'build' // update the assets path
+    },
+    target: '#svelte',
   }
 };
 
